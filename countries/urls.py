388c10f90e840d74d4e47f022f337f8d1c0a858f
abdellatif T idrissi <buyukpatron@gmail.com>
2020-12-31 15:58:27 +0100

    add requirements file to git

--- countries/urls.py
+++ countries/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url
 from countries import views
 
 urlpatterns = [
-    url(r'^api/countries$', views.countries_list),
-    url(r'^api/countries/(?P<pk>[0-9]+)$', views.countries_detail)
+    url(r'^countries$', views.countries_list),
+    url(r'^countries/(?P<pk>[0-9]+)$', views.countries_detail)
 
 ]
--- worldCountries/urls.py
+++ worldCountries/urls.py
@@ -5,5 +5,5 @@ from django.conf.urls import url, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    url(r'^', include('countries.urls')),
+    url(r'^api/', include('countries.urls')),
 ]
